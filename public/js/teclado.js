f7873a0940fd9ae6f66d8d8df700ad43603dca22
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2017-08-15 03:44:36 -0300

    Panel de configuración de los tiempos de espera

--- public/js/teclado.js
+++ public/js/teclado.js
@@ -1,17 +1,28 @@
 // https://github.com/cozyt/softkey/blob/79c80ff2b602d3759e3b789ab636cff2f4104088/index.html
-$(document).ready(function(){
-  $('.teclado').softkeys({
-    target : $('.teclado').data('target'),
-    layout : [
-      [
-        '7', '8', '9'
-      ], [
-        '4', '5', '6'
-      ], [
-        '1', '2', '3'
-      ], [
-        '0', 'delete'
-      ]
-    ]
+var layoutTeclado = [
+  [
+    '7', '8', '9'
+  ], [
+    '4', '5', '6'
+  ], [
+    '1', '2', '3'
+  ], [
+    '0', 'delete'
+  ]
+]
+
+// El div donde inicializar el teclado virtual
+var elemento = '<div class="teclado"></div>'
+// El elemento de 'teclado' actual
+var teclado = null
+
+$(document).on('focus', 'input.enfocable', function() {
+  $(teclado).remove()
+
+  $('.teclado-contenedor').append(elemento)
+
+  teclado = $('.teclado').softkeys({
+    target : $(this).data('target'),
+    layout : layoutTeclado
   })
 })
