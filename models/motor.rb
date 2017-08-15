711afe1e0c5ea890a9f90dc2150adbd54c002fd3
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2017-08-15 04:33:54 -0300

    ANGULOS es un integer

--- models/motor.rb
+++ models/motor.rb
@@ -72,7 +72,7 @@ class Motor
       ubicaciones = {}
 
       NIVELES.times do |nivel|
-        ubicaciones[nivel] = ANGULOS.to_a - Sobre.where(nivel: nivel).pluck(:angulo)
+        ubicaciones[nivel] = ANGULOS - Sobre.where(nivel: nivel).pluck(:angulo)
       end
 
       @libres = ubicaciones.reject { |_, v| v.empty? }
