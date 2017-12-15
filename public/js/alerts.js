99aece48c96f6c2e2e4029449f216953027a5b3e
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2017-12-15 06:03:54 -0300

    Desaparecer alertas después de 5 segundos

---
+++ public/js/alerts.js
@@ -0,0 +1,6 @@
+// Remueve alertas automáticamente después de 5 segundos
+window.setTimeout(function() {
+  $(".alert").fadeTo(500, 0).slideUp(500, function() {
+    $(this).remove(); 
+  });
+}, 5000);
