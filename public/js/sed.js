3a6a04b81d2236745fa0d212f5b082116cf792a7
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2018-01-09 01:45:43 -0300

    Bloquear links y forms con jquery-ujs

--- public/js/sed.js
+++ public/js/sed.js
@@ -1,9 +1,8 @@
-// Anular doble submit
-$(document).ready(function(){
-  $('form').preventDoubleSubmission();
-});
+// Anular doble submit y cargas/descargas con jquery-ujs:
+//
+//  data-disable-with="Enviando..."
 
 // Anular el menú contextual
-$(document).on('contextmenu', function (event) {
+$(document).on('contextmenu', function(event) {
   event.preventDefault();
 });
