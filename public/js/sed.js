7cfb9b2ecd22826b3e385e43ac46debbfe4a39ae
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2018-01-09 09:40:29 -0300

    Bloquear toda la pantalla en algunas operaciones críticas

--- public/js/sed.js
+++ public/js/sed.js
@@ -2,6 +2,13 @@
 //
 //  data-disable-with="Enviando..."
 
+// Bloquear toda la página cuando hay varias acciones posibles
+$(document).ready(function() {
+  $('.bloqueador').click(function() {
+    $.blockUI({ message: null });
+  });
+});
+
 // Anular el menú contextual
 $(document).on('contextmenu', function(event) {
   event.preventDefault();
