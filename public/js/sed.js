8a77819118752c40abb869cf535b392fb606cd05
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2018-02-01 19:47:28 -0300

    Volver al login después de 20 segundos (fixes #10)

--- public/js/sed.js
+++ public/js/sed.js
@@ -1,12 +1,19 @@
-// Anular doble submit y cargas/descargas con jquery-ujs:
-//
-//  data-disable-with="Enviando..."
-
-$(document).ready(function() {
-  // Bloquear toda la página cuando hay varias acciones posibles
+$(function() {
+  // Evitar doble submit, con jquery-ujs:
+  //
+  //   data-disable-with="Enviando..."
+  //
+  // Igualmente bloqueamos toda la página cuando hay varias acciones posibles
   $('.bloqueador').click(function() {
     $.blockUI({ message: null })
   })
+
+  // Volver al login después de 20 segundos sin interacción del cliente
+  if ($(".volver-al-login").length > 0) {
+    window.setTimeout(function() {
+      window.location.replace('/')
+    }, 20000)
+  }
 })
 
 // Anular el menú contextual
